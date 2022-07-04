4b4d60efcde8dc715540ddf247a101d59ea7c898
ᴍɪɴᴏᴏɴɢ <owow77@naver.com>
2022-07-04 17:33:15 +0900

    feat : 민영

--- health/src/main/resources/init.sql
+++
@@ -1,8 +0,0 @@
--- 회원테이블
-CREATE TABLE IF NOT EXISTS member
-(
-	member_name VARCHAR(20) COMMENT '회원 이름',
-	member_id INTEGER(4) COMMENT '회원 아이디',
-    member_password VARCHAR(200) COMMENT '회원 비밀번호',
-    create_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '가입 날짜'
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
